54a3c978bcced6d3d83bc7854965e081bac1a647
george <george@redingerdressage.com>
2009-06-20 21:48:15 -0700

    more

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,25 +1,12 @@
-set :application, "Synoptic"
-
-# If you aren't deploying to /u/apps/#{application} on the target
-# servers (which is the default), you can specify the actual location
-# via the :deploy_to variable:
- set :deploy_to, "synoptic.dressageinsight.com"
-
-# If you aren't using Subversion to manage your source code, specify
-# your SCM below:
-
+set :application, "synoptic.dressageinsight.com"
+set :deploy_to, "synoptic.dressageinsight.com"
 role :app, "synoptic.dressageinsight.com"
 role :web, "synoptic.dressageinsight.com"
 role :db,  "synoptic.dressageinsight.com", :primary => true
-
  ############################
-
  # Based on the original DreamHost deploy.rb recipe
-
  #
-
  #
-
  # GitHub settings #######################################################################################
 
  default_run_options[:pty] = true
@@ -27,23 +14,14 @@ role :db,  "synoptic.dressageinsight.com", :primary => true
 
 set :repository,  "git@github.com:georgeredinger/Synoptic.git"
  set :scm, "git"
-
  set :scm_passphrase, "pa$$w0rd" #This is the passphrase for the ssh key on the server deployed to
-
  set :branch, "master"
-
  set :scm_verbose, true
-
  #########################################################################################################
-
  set :user, 'gredinger' #Dreamhost username
-
  set :domain, 'synoptic.dressageinsight.com'  # Dreamhost servername where your account is located
-
  set :project, 'Synoptic' # Your application as its called in the repository
-
  set :application, 'synoptic.dressageinsight.com' # Your app's location (domain or sub-domain name as setup in panel)
-
  set :applicationdir, "/home/#{user}/#{application}" # The standard Dreamhost setup
 
 
@@ -54,26 +32,17 @@ set :repository,  "git@github.com:georgeredinger/Synoptic.git"
  set :deploy_to, applicationdir
 
  set :deploy_via, :remote_cache
-  
 
  # roles (servers)
-
  role :app, domain
-
  role :web, domain
-
  role :db, domain, :primary => true
-
   
 
  namespace :deploy do
-
  [:start, :stop, :restart, :finalize_update, :migrate, :migrations, :cold].each do |t|
-
- desc "#{t} task is a no-op with mod_rails"
-
- task t, :roles => :app do ; end
-
+   desc "#{t} task is a no-op with mod_rails"
+   task t, :roles => :app do ; end
  end
 
  end
